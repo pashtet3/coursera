6c09af71fbcfb48430227ae634b4f86ca734e36b
pashtet3 <mega_pachtet@ukr.net>
2020-04-06 00:06:03 +0300

    task first week

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
 import './App.css';
+import Menu from './components/MenuComponent';
+import DishDetail from './components/DishdetailComponent'
 import { DISHES } from './shared/dishes';
 
-class App extends Component {
-    constructor (props)  {
-        super(props);
+import { Navbar, NavbarBrand } from 'reactstrap';
 
+class App extends Component {
+
+    constructor(props) {
+        super(props);
         this.state = {
-            dishes: DISHES
+            dishes: DISHES,
+            selectedDishID: null
         };
     }
-    render (){
-    return (
-      <div>
-       < Navbar dark color="primary">
-        <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-         </div>
-           </Navbar>
-            <Menu dishes={this.state.dishes} />
-      </div>
-);
-    }  
+
+    onDishSelect(ID){
+        this.setState({selectedDishID: ID})
+    }
+
+    render() {
+        return (
+          <div className="App">
+            <Navbar dark color="primary">
+              <div className="container">
+                <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+              </div>
+            </Navbar>
+             <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDishID)[0]} />      </div>
+        );
+            }
 }
 
 export default App;
