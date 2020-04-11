e5827f5b7d80dceea2d137bd5aefaebb338ede98
pashtet3 <mega_pachtet@ukr.net>
2020-04-11 20:13:14 +0300

    React Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
-import DishDetail from './components/DishdetailComponent'
-import { Navbar, NavbarBrand } from 'reactstrap';
-
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 
 class App extends Component {
 
     render() {
         return (
-          <div className="App">
-            <Main />
-          </div>
+          <BrowserRouter>
+      <div className="App">
+        <Main />
+      </div>
+    </BrowserRouter>
       );
     }
 }
