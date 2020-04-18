52ee447b73a60e306767f8c6000f6f7610533306
pashtet3 <mega_pachtet@ukr.net>
2020-04-18 19:35:23 +0300

    2 week

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -7,13 +8,11 @@ import DishDetail from './DishdetailComponent';
 import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-
 class Main extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
@@ -23,8 +22,6 @@ class Main extends Component {
             leaders: LEADERS
         };
     }
-
-    
     render() {
         const HomePage = () => {
             return (
@@ -34,19 +31,18 @@ class Main extends Component {
                 />
             );
         }
-
         const DishWithId = ({match}) => {
             return(
                 <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
                   comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
             );
-          };
-        
+          }; 
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
+                    <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders}/>} /> 
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
@@ -57,5 +53,4 @@ class Main extends Component {
         );
     }
 }
-
 export default Main;
